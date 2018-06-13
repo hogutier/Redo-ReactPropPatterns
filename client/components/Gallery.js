0965d846ac59fc8a70e2f4e60946991046806fd0
Horacio Gutierrez <hogutier@yahoo.com>
2018-06-13 10:08:47 -0500

    complete exercise

--- client/components/Gallery.js
+++ client/components/Gallery.js
@@ -2,13 +2,18 @@ import React, { Component } from "react";
 
 class Galery extends Component {
   render() {
-    const { children } = this.props;
+    const { title, children, author } = this.props;
     return (
       <div className="galery">
+        <header>{title} by {author} - {React.Children.count(children)} images</header>
         <div className="content">{children}</div>
       </div>
     );
   }
+}
+
+Galery.defaultProps = {
+  author: 'Anonymous'
 }
 
 export default Galery;
